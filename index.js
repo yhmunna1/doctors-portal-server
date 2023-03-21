7796c820cb3941e5592fd3c20d967b0984012f15
yhmunna1 <yh.munna.barisal@gmail.com>
2023-03-22 01:22:52 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qjz282s.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        // console.log('Database Connected');
+        const servicesCollection = client.db("doctors_portal").collection("services");
+
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = servicesCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+    }
+    finally {
+        ;
+    }
+};
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello From Doctors Portal!')
+})
+
+app.listen(port, () => {
+    console.log(`Doctors app listening on port ${port}`)
+})
